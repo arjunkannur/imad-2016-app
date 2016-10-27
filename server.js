18c5f65643e005bffc47a96fa2da61f87d180095
arjunkannur <arjunwebdesigner4@gmail.com>
2016-10-27 18:27:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var articleone = {
 };
 function createtemplate(data)
 {
-     var title=data.tiltle;
+     var title=data.title;
      var heading=data.heading;
      var date=data.date;
      var content=data.content;
