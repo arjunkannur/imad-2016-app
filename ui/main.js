f1bb78458a733148f74eccd67f2ddb4d24135646
arjunkannur <arjunwebdesigner4@gmail.com>
2016-10-28 09:01:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var img=document.getElementById("moving");
+function moveright(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft;
+}
+img.onclick = function (){
+    var intervel= setIntervel(moveright,100);
+};
